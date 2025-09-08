04b94cc76d7c6851a0ad0ce9043f65b6a8b758de
Cassidy-02 <cassidymotto15@gmail.com>
2025-09-08 16:15:00 +0200

    Add UI components and event handlers to Form1
    Updated `Form1.Designer.cs` to include new buttons and labels for reporting issues, viewing events, checking service status, and exiting the application. Adjusted layout and form size.
    
    Implemented event handlers in `Form1.cs` for the new buttons, enabling functionality for opening related forms and exiting the application.
    
    Modified `Form1.resx` to include resource definitions in XML format for better resource management.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReportIssues = new System.Windows.Forms.Button();
+            this.btnEvents = new System.Windows.Forms.Button();
+            this.btnStatus = new System.Windows.Forms.Button();
+            this.btn_StatusIssues = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Maiandra GD", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(331, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Task";
+            // 
+            // btnReportIssues
+            // 
+            this.btnReportIssues.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportIssues.Location = new System.Drawing.Point(357, 164);
+            this.btnReportIssues.Name = "btnReportIssues";
+            this.btnReportIssues.Size = new System.Drawing.Size(163, 51);
+            this.btnReportIssues.TabIndex = 1;
+            this.btnReportIssues.Text = "Report Issues";
+            this.btnReportIssues.UseVisualStyleBackColor = true;
+            this.btnReportIssues.Click += new System.EventHandler(this.btnReportIssues_Click);
+            // 
+            // btnEvents
+            // 
+            this.btnEvents.Enabled = false;
+            this.btnEvents.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEvents.Location = new System.Drawing.Point(357, 254);
+            this.btnEvents.Name = "btnEvents";
+            this.btnEvents.Size = new System.Drawing.Size(163, 49);
+            this.btnEvents.TabIndex = 2;
+            this.btnEvents.Text = "Local Events and Announcements";
+            this.btnEvents.UseVisualStyleBackColor = true;
+            this.btnEvents.Click += new System.EventHandler(this.btnEvents_Click);
+            // 
+            // btnStatus
+            // 
+            this.btnStatus.Enabled = false;
+            this.btnStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStatus.Location = new System.Drawing.Point(357, 347);
+            this.btnStatus.Name = "btnStatus";
+            this.btnStatus.Size = new System.Drawing.Size(163, 51);
+            this.btnStatus.TabIndex = 3;
+            this.btnStatus.Text = "Service Request Status";
+            this.btnStatus.UseVisualStyleBackColor = true;
+            this.btnStatus.Click += new System.EventHandler(this.btnStatus_Click);
+            // 
+            // btn_StatusIssues
+            // 
+            this.btn_StatusIssues.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_StatusIssues.Location = new System.Drawing.Point(597, 164);
+            this.btn_StatusIssues.Name = "btn_StatusIssues";
+            this.btn_StatusIssues.Size = new System.Drawing.Size(140, 51);
+            this.btn_StatusIssues.TabIndex = 0;
+            this.btn_StatusIssues.Text = "Reported Issues";
+            this.btn_StatusIssues.UseVisualStyleBackColor = true;
+            this.btn_StatusIssues.Click += new System.EventHandler(this.btn_StatusIssues_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(684, 436);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(186, 43);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit Application";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(958, 515);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btn_StatusIssues);
+            this.Controls.Add(this.btnStatus);
+            this.Controls.Add(this.btnEvents);
+            this.Controls.Add(this.btnReportIssues);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
+
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReportIssues;
+        private System.Windows.Forms.Button btnEvents;
+        private System.Windows.Forms.Button btnStatus;
+        private System.Windows.Forms.Button btn_StatusIssues;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,38 @@ namespace MunicipalServiceApp
         {
             InitializeComponent();
         }
+
+        private void btnReportIssues_Click(object sender, EventArgs e)
+        {
+            ReportIssuesForm reportForm = new ReportIssuesForm(this);
+            this.Hide();
+            reportForm.ShowDialog();
+            if (!this.IsDisposed && !this.Disposing)
+                this.Show();
+        }
+
+        private void btnEvents_Click(object sender, EventArgs e)
+        {
+            //Implement in Part 2
+        }
+
+        private void btnStatus_Click(object sender, EventArgs e)
+        {
+            //Implement in Part 3
+        }
+
+        private void btn_StatusIssues_Click(object sender, EventArgs e)
+        {
+            StatusForm statusForm = new StatusForm(this);
+            this.Hide();
+            statusForm.ShowDialog();
+            if (!this.IsDisposed && !this.Disposing)
+                this.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+           Application.Exit();
+        }
     }
 }
