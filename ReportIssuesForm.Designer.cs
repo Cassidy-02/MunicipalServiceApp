827bef0666ea5f2bb50326dd63a0a5f1e29b580a
Cassidy-02 <cassidymotto15@gmail.com>
2025-09-08 16:16:02 +0200

    Add ReportIssuesForm with UI and logic implementation
    Updated `ReportIssuesForm.Designer.cs` to define the layout and components, including text boxes, labels, buttons, and a progress bar. Implemented event handlers in `ReportIssuesForm.cs` for media upload, report submission, and navigation, along with user input validation and progress simulation. Modified `ReportIssuesForm.resx` to include resource definitions for the form's UI elements and error provider settings.

---
+++ ReportIssuesForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace MunicipalServiceApp
+{
+    partial class ReportIssuesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listCategory = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.richtxtDescription = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(373, 42);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(329, 22);
+            this.txtLocation.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(102, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Location of reported issue:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(102, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Category ";
+            // 
+            // listCategory
+            // 
+            this.listCategory.FormattingEnabled = true;
+            this.listCategory.ItemHeight = 16;
+            this.listCategory.Items.AddRange(new object[] {
+            "Water",
+            "Electriicty",
+            "Roads",
+            "Drains"});
+            this.listCategory.Location = new System.Drawing.Point(256, 101);
+            this.listCategory.Name = "listCategory";
+            this.listCategory.Size = new System.Drawing.Size(197, 52);
+            this.listCategory.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(102, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(102, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(233, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Upload media attachment";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Location = new System.Drawing.Point(390, 282);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(108, 34);
+            this.btnUpload.TabIndex = 7;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(430, 397);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(128, 41);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(237, 397);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(135, 41);
+            this.btnBack.TabIndex = 9;
+            this.btnBack.Text = "Back To Menu";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(585, 250);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 128);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(268, 346);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(230, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // richtxtDescription
+            // 
+            this.richtxtDescription.Location = new System.Drawing.Point(349, 171);
+            this.richtxtDescription.Name = "richtxtDescription";
+            this.richtxtDescription.Size = new System.Drawing.Size(197, 96);
+            this.richtxtDescription.TabIndex = 12;
+            this.richtxtDescription.Text = "";
+            this.richtxtDescription.TextChanged += new System.EventHandler(this.richtxtDescription_TextChanged);
+            // 
+            // ReportIssuesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richtxtDescription);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listCategory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLocation);
+            this.Name = "ReportIssuesForm";
+            this.Text = "ReportIssuesForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.RichTextBox richtxtDescription;
+    }
+}
---
+++ ReportIssuesForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MunicipalServiceApp
+{
+    public partial class ReportIssuesForm : Form
+    {
+        private Form1 _mainForm;
+        public ReportIssuesForm(Form1 mainForm)
+        {
+            InitializeComponent();
+            _mainForm = mainForm;
+        }
+
+        private void ReportIssuesForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _mainForm.Show();
+        }
+
+        //Upload media button
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Filter = "Image Files|*.jpg;*.jpeg;*.png|All Files|*.*";
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openfile.FileName);
+            }
+        }
+
+        // Submit report button
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // clear old errors
+            errorProvider1.Clear();
+            bool hasError = false;
+
+            //Check category
+            if (listCategory.SelectedItem == null)
+            {
+                errorProvider1.SetError(listCategory, "Please select a category.");
+                hasError = true;
+            }
+
+            //Check description
+            if (string.IsNullOrWhiteSpace(richtxtDescription.Text))
+            {
+                errorProvider1.SetError(richtxtDescription, "Description cannot be empty.");
+                hasError = true;
+            }
+
+            //Check location
+            if (string.IsNullOrWhiteSpace(txtLocation.Text))
+            {
+                errorProvider1.SetError(txtLocation, "Location cannot be empty.");
+                hasError = true;
+            }
+            if (hasError) return; //stop submission if there are errors
+
+            var issue = new Issue
+            {
+                Category = listCategory.SelectedItem.ToString(),
+                Description = richtxtDescription.Text.Trim(),
+                Location = txtLocation.Text.Trim(),
+                DateReported = DateTime.Now
+            };
+
+            //Add to repository (adds to list + file)
+            IssueRepository.AddIssue(issue);
+
+
+            //Simulate progress
+            progressBar1.Value = 0;
+            for (int i = 0; i <= 100; i += 10)
+            {
+                progressBar1.Value = i; 
+                System.Threading.Thread.Sleep(50); //small delay to simulate progress
+                Application.DoEvents(); // refresh UI
+            }
+            MessageBox.Show("Thank you for reporting this issue!\n\nYour feedback helps improve services.","Report Submitted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Closes the form and returns to main
+            this.BeginInvoke(new Action(() => this.DialogResult = DialogResult.Cancel));
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Form1 mainForm = new Form1();
+            this.Hide();
+            mainForm.ShowDialog();
+            this.Show();
+        }
+
+        private void richtxtDescription_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
