c51d381504eab361558ddf6abed1f8f11cebab71
Cassidy-02 <cassidymotto15@gmail.com>
2025-09-20 18:11:34 +0200

    This class is the core data object. All events displayed in the UI, stored in the priority queue, and organized by category are instances oooooooooooooooof this class.

---
+++ Event.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunicipalServiceApp
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Date.ToShortDateString()} - {Name} ({Category}) @ {Location}";
+          
+        }
+    }
+}
