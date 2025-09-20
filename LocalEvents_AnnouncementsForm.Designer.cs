27be04323cacd6fb9b9a2c2482e0ded569582c06
Cassidy-02 <cassidymotto15@gmail.com>
2025-09-20 18:16:32 +0200

    Implemented LocalEvents_Announcements module
    - Added Event class to represent individual events
    - Integrated PriorityQueue to manage urgent events efficiently
    - Added LocalEvents_AnnocumentsForm with ListView, filtering by category and date
    - Used SortedDictionary to organize events by category
    - Added HashSet for unique categories and dates
    - Connected DateTimePicker for date filtering
    - Implemented Urgent Event button to show earliest upcoming event
    - Ensured navigation from main form (Form1) to events form

---
+++ LocalEvents_AnnouncementsForm.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace MunicipalServiceApp
+{
+    partial class LocalEvents_AnnocumentsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnUrgent = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listViewEvents = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(747, 69);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Local Events & Announcements";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnUrgent);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBoxCategory);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(747, 90);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Events";
+            // 
+            // btnUrgent
+            // 
+            this.btnUrgent.Location = new System.Drawing.Point(7, 61);
+            this.btnUrgent.Name = "btnUrgent";
+            this.btnUrgent.Size = new System.Drawing.Size(110, 23);
+            this.btnUrgent.TabIndex = 5;
+            this.btnUrgent.Text = "Soonest Event";
+            this.btnUrgent.UseVisualStyleBackColor = true;
+            this.btnUrgent.Click += new System.EventHandler(this.btnUrgent_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(578, 30);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(115, 30);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(329, 35);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(206, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "From Date:";
+            // 
+            // comboBoxCategory
+            // 
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Items.AddRange(new object[] {
+            "All",
+            "Community",
+            "Music ",
+            "Sports"});
+            this.comboBoxCategory.Location = new System.Drawing.Point(93, 34);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxCategory.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Category";
+            // 
+            // listViewEvents
+            // 
+            this.listViewEvents.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listViewEvents.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewEvents.FullRowSelect = true;
+            this.listViewEvents.HideSelection = false;
+            this.listViewEvents.Location = new System.Drawing.Point(0, 159);
+            this.listViewEvents.Name = "listViewEvents";
+            this.listViewEvents.Size = new System.Drawing.Size(747, 291);
+            this.listViewEvents.TabIndex = 2;
+            this.listViewEvents.UseCompatibleStateImageBehavior = false;
+            this.listViewEvents.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Event Name";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Category";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Location";
+            this.columnHeader4.Width = 150;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Description";
+            this.columnHeader5.Width = 300;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(747, 26);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(103, 20);
+            this.toolStripStatusLabel1.Text = "Total Events: 0";
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Location = new System.Drawing.Point(586, 424);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(149, 28);
+            this.btnMenu.TabIndex = 4;
+            this.btnMenu.Text = "Back to Menu";
+            this.btnMenu.UseVisualStyleBackColor = true;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // LocalEvents_AnnocumentsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 450);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listViewEvents);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "LocalEvents_AnnocumentsForm";
+            this.Text = "LocalEvents_AnnocumentsForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxCategory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listViewEvents;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.Button btnUrgent;
+    }
+}
---
+++ LocalEvents_AnnouncementsForm.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MunicipalServiceApp
+{
+    public partial class LocalEvents_AnnocumentsForm : Form
+    {
+        private PriorityQueue<Event> eventQueue = new PriorityQueue<Event>();
+
+        // SortedDictionary for automatic date sorting
+        private SortedDictionary<string, List<Event>> eventDictionary = new SortedDictionary<string, List<Event>>();
+        
+        // Stores unique event categories
+        private HashSet<string> eventCategories = new HashSet<string>();
+
+        // Stores unique event dates (just the date part)
+        private HashSet<DateTime> eventDates = new HashSet<DateTime>();
+
+        // Reference to main form
+        private Form1 _mainForm;
+        public LocalEvents_AnnocumentsForm(Form1 mainForm)
+        {
+            InitializeComponent();
+           _mainForm = mainForm;
+
+
+            
+            // Load test data
+            LoadEvents();
+            DisplayEvents(eventDictionary.SelectMany(kvp => kvp.Value).ToList());
+            LoadedCategories();
+            LoadAvailableDates();
+
+        }
+
+        // Load Events
+        private void LoadEvents()
+        {
+            AddEvent(new Event { Name = "Community Cleanup", Date = DateTime.Now.AddDays(1), Location = "Helderberg Park", Description = "Join us for a community cleanup event at the local park.", Category = "Community" });
+            AddEvent(new Event { Name = "Community Meeting", Date = DateTime.Now.AddDays(2), Location = "Town Hall", Description = "Monthly community update", Category = "Community" });
+            AddEvent(new Event { Name = "Concert", Date = DateTime.Now.AddDays(1), Location = "City Park", Description = "Live music event", Category = "Music" });
+            AddEvent(new Event { Name = "Food Festival", Date = DateTime.Now.AddDays(5), Location = "Downtown", Description = "Taste dishes from around the world", Category = "Festival" });
+            AddEvent(new Event { Name = "Tech Workshop", Date = DateTime.Now.AddDays(3), Location = "Library", Description = "Intro to programming workshop", Category = "Education" });
+        }
+
+        private void AddEvent (Event ev)
+        {
+            // Add to Priority Queue (priority based on date)
+            int priority = (ev.Date - DateTime.Now).Days;
+            if (priority < 0) priority = int.MaxValue;  // Past event goes to the end
+
+            eventQueue.Enqueue(ev, priority);
+
+            // Add to SortedDictionary
+            if (!eventDictionary.ContainsKey(ev.Category))
+            {
+                eventDictionary[ev.Category] = new List<Event>();
+            }
+
+            eventDictionary[ev.Category].Add(ev);
+
+            // Update sets
+            eventCategories.Add(ev.Category);
+            eventDates.Add(ev.Date.Date); // only date part
+        }
+
+        // Display a list of events in ListView
+        private void DisplayEvents(List<Event> events)
+        {
+            listViewEvents.Items.Clear();
+
+            var snapshot = eventQueue.Snapshot();
+            snapshot.Sort((a, b) => a.Priority.CompareTo(b.Priority));
+
+            foreach (var entry in snapshot)
+            {
+                Event ev = entry.Item;
+                ListViewItem item = new ListViewItem(ev.Name);
+                item.SubItems.Add(ev.Date.ToShortDateString());
+                item.SubItems.Add(ev.Category);
+                item.SubItems.Add(ev.Location);
+                item.SubItems.Add(ev.Description);
+                listViewEvents.Items.Add(item);
+            }
+            label3.Text = $"Total Events:{listViewEvents.Items.Count}";
+        }
+
+        private void LoadedCategories()
+        {
+            comboBoxCategory.Items.Clear();
+            foreach (var category in eventCategories)
+            {
+                comboBoxCategory.Items.Add(category);
+            }
+        }
+
+        private void LoadEvents_Announcements_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _mainForm.Show();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            
+            string category = comboBoxCategory.SelectedItem?.ToString();
+            DateTime selectedDate = dateTimePicker1.Value.Date;
+
+            listViewEvents.Items.Clear();
+
+            IEnumerable<Event> results;
+
+            if (!string.IsNullOrEmpty(category) && eventDictionary.ContainsKey(category))
+            {
+                results = eventDictionary[category];
+            }
+            else
+            {
+                results = eventDictionary.Values.SelectMany(list => list);
+            }
+
+            results = results.Where(ev =>
+            category == "All" || ev.Category.Equals(category, StringComparison.OrdinalIgnoreCase)).OrderBy(ev => ev.Date);
+            
+
+            foreach (var ev in results.OrderBy(ev => ev.Date))
+            {
+                ListViewItem item = new ListViewItem(ev.Name);
+                item.SubItems.Add(ev.Date.ToShortDateString());
+                item.SubItems.Add(ev.Category);
+                item.SubItems.Add(ev.Location);
+                item.SubItems.Add(ev.Description);
+                listViewEvents.Items.Add(item);
+
+            }
+
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            Form1 mainForm = new Form1();
+            this.Hide();
+            mainForm.ShowDialog();
+            this.Show();
+        }
+
+        // Event Button (Priority Queue)
+        private void btnUrgent_Click(object sender, EventArgs e)
+        {
+            if (eventQueue.Count > 0)
+            {
+                Event urgentEvent = eventQueue.Dequeue();
+                MessageBox.Show($"Most urgent event:\n{urgentEvent.Name}\nDate:{urgentEvent.Date.ToShortDateString()}",
+                    "Urgent Event");
+            }
+            else
+            {
+                MessageBox.Show("No urgent events available.", "Urgent Event");
+            }
+        }
+
+        // Load available dates into DateTimePicker
+        private void LoadAvailableDates()
+        {
+            if (eventDates.Count > 0)
+            {
+                dateTimePicker1.MinDate = eventDates.Min();
+                dateTimePicker1.MaxDate = eventDates.Max();
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime selecteddate = dateTimePicker1.Value.Date;
+
+            if (eventDates.Contains(selecteddate))
+            {
+                 DateTime selectedDate = dateTimePicker1.Value.Date;
+
+                if (eventDates.Contains(selectedDate))
+                {
+                    var eventsDate = eventDictionary
+                        .SelectMany(kvp => kvp.Value)
+                        .Where(ev => ev.Date.Date == selectedDate)
+                        .ToList();
+
+                    DisplayEvents(eventsDate);
+                }
+            }
+        }
+    }
+}
