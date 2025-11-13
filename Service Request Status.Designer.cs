89116b444ce2233768813f705e0a634ae081d88c
Cassidy-02 <cassidymotto15@gmail.com>
2025-11-13 20:33:39 +0200

    Designed a page called Service request Status.

---
+++ Service Request Status.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace MunicipalServiceApp
+{
+    partial class Service_Request_Status
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewRequests = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtSearchID = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMostUrgent = new System.Windows.Forms.Button();
+            this.btnViewDependencies = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewRequests
+            // 
+            this.listViewRequests.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listViewRequests.FullRowSelect = true;
+            this.listViewRequests.GridLines = true;
+            this.listViewRequests.HideSelection = false;
+            this.listViewRequests.Location = new System.Drawing.Point(43, 82);
+            this.listViewRequests.Name = "listViewRequests";
+            this.listViewRequests.Size = new System.Drawing.Size(638, 300);
+            this.listViewRequests.TabIndex = 0;
+            this.listViewRequests.UseCompatibleStateImageBehavior = false;
+            this.listViewRequests.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "RequestID";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "RequestType";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "DateSubmitted";
+            this.columnHeader4.Width = 150;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Priority";
+            this.columnHeader5.Width = 100;
+            // 
+            // txtSearchID
+            // 
+            this.txtSearchID.Location = new System.Drawing.Point(200, 49);
+            this.txtSearchID.Name = "txtSearchID";
+            this.txtSearchID.Size = new System.Drawing.Size(137, 22);
+            this.txtSearchID.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(375, 47);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 27);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search ID";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(480, 47);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(90, 27);
+            this.btnShowAll.TabIndex = 3;
+            this.btnShowAll.Text = "Show All";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(120, 20);
+            this.toolStripStatusLabel1.Text = "Total Requests: 0";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter Request ID:";
+            // 
+            // btnMostUrgent
+            // 
+            this.btnMostUrgent.Location = new System.Drawing.Point(687, 98);
+            this.btnMostUrgent.Name = "btnMostUrgent";
+            this.btnMostUrgent.Size = new System.Drawing.Size(87, 31);
+            this.btnMostUrgent.TabIndex = 6;
+            this.btnMostUrgent.Text = "Urgent";
+            this.btnMostUrgent.UseVisualStyleBackColor = true;
+            this.btnMostUrgent.Click += new System.EventHandler(this.btnMostUrgent_Click);
+            // 
+            // btnViewDependencies
+            // 
+            this.btnViewDependencies.Location = new System.Drawing.Point(687, 159);
+            this.btnViewDependencies.Name = "btnViewDependencies";
+            this.btnViewDependencies.Size = new System.Drawing.Size(87, 29);
+            this.btnViewDependencies.TabIndex = 8;
+            this.btnViewDependencies.Text = "View Dependencies";
+            this.btnViewDependencies.UseVisualStyleBackColor = true;
+            this.btnViewDependencies.Click += new System.EventHandler(this.btnViewDependencies_Click);
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Location = new System.Drawing.Point(324, 388);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(123, 37);
+            this.btnMenu.TabIndex = 9;
+            this.btnMenu.Text = "Back to Menu";
+            this.btnMenu.UseVisualStyleBackColor = true;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // Service_Request_Status
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.btnViewDependencies);
+            this.Controls.Add(this.btnMostUrgent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchID);
+            this.Controls.Add(this.listViewRequests);
+            this.Name = "Service_Request_Status";
+            this.Text = "Service Request Status";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewRequests;
+        private System.Windows.Forms.TextBox txtSearchID;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMostUrgent;
+        private System.Windows.Forms.Button btnViewDependencies;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnMenu;
+    }
+}
---
+++ Service Request Status.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MunicipalServiceApp
+{
+    public partial class Service_Request_Status : Form
+    {
+        private BinarySearchTree bst = new BinarySearchTree();
+        private MinHeap heap = new MinHeap();
+        private Graph graph = new Graph();
+
+        // Reference to main form
+        private Form1 _mainForm;
+
+        public Service_Request_Status(Form1 form1)
+        {
+            _mainForm = form1;
+            InitializeComponent();
+            LoadRequests();
+            DisplayRequests();
+        }
+
+       
+
+        private void LoadRequests()
+        {
+            List<ServiceRequest> requests = new List<ServiceRequest>
+            {
+                new ServiceRequest { RequestID = 1, RequestType = "Water Leak", Status = "Pending", DateSubmitted = DateTime.Now.AddDays(-2), Priority = 2 },
+                new ServiceRequest { RequestID = 2, RequestType = "Electric Fault", Status = "In Progress", DateSubmitted = DateTime.Now.AddDays(-1), Priority = 1 },
+                new ServiceRequest { RequestID = 3, RequestType = "Waste Collection", Status = "Completed", DateSubmitted = DateTime.Now.AddDays(-5), Priority = 3 },
+                new ServiceRequest { RequestID = 4, RequestType = "Road Repair", Status = "Pending", DateSubmitted = DateTime.Now.AddDays(-3), Priority = 2 },
+                new ServiceRequest { RequestID = 5, RequestType = "Streetlight Repair", Status = "In Progress", DateSubmitted = DateTime.Now.AddDays(-4), Priority = 1 }           
+        };
+
+            foreach (var r in requests)
+            {
+                bst.Insert(r);
+                heap.Insert(r);
+            }
+
+            DisplayRequests();
+        }
+
+        private void DisplayRequests()
+        {
+            listViewRequests.Items.Clear();
+            foreach (var req in bst.InOrderTraversal())
+            {
+                var item = new ListViewItem(req.RequestID.ToString());
+                item.SubItems.Add(req.RequestType);
+                item.SubItems.Add(req.Status);
+                item.SubItems.Add(req.DateSubmitted.ToString("yyyy-MM-dd"));
+                item.SubItems.Add(req.Priority.ToString());
+                listViewRequests.Items.Add(item);
+            }
+
+            toolStripStatusLabel1.Text = $"Total Requests: {listViewRequests.Items.Count}";
+        }
+
+        private void btnMostUrgent_Click(object sender, EventArgs e)
+        {
+            var urgent = heap.ExtractMin();
+            if (urgent != null)
+                MessageBox.Show($"Most urgent: {urgent.RequestType}", "Urgent Request");
+            else
+                MessageBox.Show("No requests available.");
+        }
+
+        private void btnViewDependencies_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtSearchID.Text, out int id))
+            {
+                var deps = graph.GetDependencies(id);
+                if (deps.Count > 0)
+                    MessageBox.Show($"Request {id} depends on: {string.Join(", ", deps)}");
+                else
+                    MessageBox.Show("No dependencies found.");
+            }
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Form1 mainForm = new Form1();
+                this.Hide();
+                mainForm.ShowDialog();
+                this.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error returning to main menu: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtSearchID.Text))
+                {
+                    MessageBox.Show("Please enter a Request ID to search.", "Input Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!int.TryParse(txtSearchID.Text, out int searchid))
+                {
+                    MessageBox.Show("Invalid Request ID format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtSearchID.Clear();
+                    return;
+                }
+
+                ServiceRequest foundRequest = bst.Search(searchid);
+
+                listViewRequests.Items.Clear();
+
+                if (foundRequest != null)
+                {
+                    var item = new ListViewItem(foundRequest.RequestID.ToString());
+                    item.SubItems.Add(foundRequest.RequestType);
+                    item.SubItems.Add(foundRequest.Status);
+                    item.SubItems.Add(foundRequest.DateSubmitted.ToString("yyyy-MM-dd"));
+                    item.SubItems.Add(foundRequest.Priority.ToString());
+                    listViewRequests.Items.Add(item);
+
+                    toolStripStatusLabel1.Text = $"Request {searchid} found.";
+                    MessageBox.Show($"Request {foundRequest.RequestID} found successfully!", "Search Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Request ID {searchid} not found.", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    toolStripStatusLabel1.Text = $"Request {searchid} not found.";
+                }
+                // Clear search box after search
+                txtSearchID.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during search: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSearchID.Clear();
+            }
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+           LoadRequests();
+            txtSearchID.Clear();
+        }
+    }
+}
